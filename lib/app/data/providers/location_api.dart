a5309ffaf7bb61e1a55f4761ae2bda6e168c1886
StaticReflection <deng3616888303@gmail.com>
2024-09-23 21:09:10 +0800

    fix: 断网时选择位置崩溃

--- lib/app/data/providers/location_api.dart
+++ lib/app/data/providers/location_api.dart
@@ -7,28 +7,33 @@ class LocationApi {
   LocationApi();
 
   Future<List<Location>?> getLocationList({String? provinceCode}) async {
-    Request request;
-    if (provinceCode == null) {
-      request = Request(host: Hosts.provinceList);
-    } else {
-      request = Request(host: Hosts.cityList(provinceCode));
-    }
+    try {
+      Request request;
+      if (provinceCode == null) {
+        request = Request(host: Hosts.provinceList);
+      } else {
+        request = Request(host: Hosts.cityList(provinceCode));
+      }
 
-    Map map = await request.fetch();
-    RegExp pattern = RegExp(r'(\w+),([\u4e00-\u9fa5]+)');
-    Iterable<RegExpMatch> regExpMatch = pattern.allMatches(map['data']);
+      Map map = await request.fetch();
+      RegExp pattern = RegExp(r'(\w+),([\u4e00-\u9fa5]+)');
+      Iterable<RegExpMatch> regExpMatch = pattern.allMatches(map['data']);
 
-    if (regExpMatch.first.group(1) == null) {
-      log.error('未找到匹配项: $map');
+      if (regExpMatch.first.group(1) == null) {
+        log.error('未找到匹配项: $map');
+        return null;
+      }
+
+      List<Location> locationList = [];
+      for (var element in regExpMatch) {
+        locationList
+            .add(Location(code: element.group(1)!, name: element.group(2)!));
+      }
+
+      return locationList;
+    } catch (e) {
+      log.error(e);
       return null;
     }
-
-    List<Location> locationList = [];
-    for (var element in regExpMatch) {
-      locationList
-          .add(Location(code: element.group(1)!, name: element.group(2)!));
-    }
-
-    return locationList;
   }
 }
